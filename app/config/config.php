8aabe760fd9216f0fdce3c236b96cd40063c03d9
gurparshad <panksharma230@gmail.com>
2021-01-14 20:17:32 +0200

    db configuration

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,9 @@
 <?php
 //Database params
-define('DB_HOST', 'localhost'); //Add your db host
-define('DB_USER', 'root'); // Add your DB root
-define('DB_PASS', ''); //Add your DB pass
-define('DB_NAME', 'scandiweb'); //Add your DB Name
+define('DB_HOST', 'us-cdbr-east-03.cleardb.com'); //Add your db host
+define('DB_USER', 'bfb34bad462bd6'); // Add your DB root
+define('DB_PASS', '7f97b45f'); //Add your DB pass
+define('DB_NAME', 'heroku_95b75a07064bf61'); //Add your DB Name
 
 //APPROOT
 define('APPROOT', dirname(dirname(__FILE__)));
